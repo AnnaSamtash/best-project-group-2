ba6e8855b45365babe01fc541db6bc6b3488b7c7
AnnaSamtash <sansara6900@gmail.com>
2023-12-22 16:57:07 +0300

    open modal-help

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-help-open]'),
+    closeModalBtn: document.querySelector('[data-modal-help-close]'),
+    modal: document.querySelector('[data-modal-help]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
