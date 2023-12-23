8c3f068c3fd7ed0106240eea5005152f69e40e56
AnnaSamtash <sansara6900@gmail.com>
2023-12-23 13:33:43 +0300

    add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,7 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal-help]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
