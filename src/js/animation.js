590c8e4098c7e82b8f5d74801a34585d07663408
OlhaArtemenko <global14072012@gmail.com>
2023-12-24 12:54:35 +0200

    animation our yachts
    анімація карток і кнопки

--- src/js/animation.js
+++ src/js/animation.js
@@ -5,3 +5,11 @@ document.querySelector('.hero-btn').onmousemove = e => {
   e.target.style.setProperty('--x', `${x}px`);
   e.target.style.setProperty('--y', `${y}px`);
 };
+
+document.querySelector('.yaht-rental-link-button').onmousemove = e => {
+  const x = e.pageX - e.target.offsetLeft;
+  const y = e.pageY - e.target.offsetTop;
+
+  e.target.style.setProperty('--x', `${x}px`);
+  e.target.style.setProperty('--y', `${y}px`);
+};
