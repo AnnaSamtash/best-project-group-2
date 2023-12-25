8a66e49f9bff371ed65ec9f33d76a7356a9b882b
AnnaSamtash <sansara6900@gmail.com>
2023-12-25 22:18:21 +0300

    our-yaht update

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,12 +1,5 @@
-document.querySelector('.hero-btn').onmousemove = e => {
-  const x = e.pageX - e.target.offsetLeft;
-  const y = e.pageY - e.target.offsetTop;
 
-  e.target.style.setProperty('--x', `${x}px`);
-  e.target.style.setProperty('--y', `${y}px`);
-};
-
-document.querySelector('.yaht-rental-link-button').onmousemove = e => {
+document.querySelector('.yaht-rental-link').onmousemove = e => {
   const x = e.pageX - e.target.offsetLeft;
   const y = e.pageY - e.target.offsetTop;
 
