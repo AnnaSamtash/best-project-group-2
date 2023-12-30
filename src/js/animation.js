b7453a470bc3c7fa2a855c11b7653b7ea9595224
AnnaSamtash <sansara6900@gmail.com>
2023-12-30 14:39:48 +0300

    change

--- src/js/animation.js
+++ src/js/animation.js
@@ -7,10 +7,3 @@ document.querySelector('.yaht-rental-link').onmousemove = e => {
   e.target.style.setProperty('--y', `${y}px`);
 };
 
-// document.querySelector('.hero-btn').onmousemove = e => {
-//   const x = e.pageX - e.target.offsetLeft;
-//   const y = e.pageY - e.target.offsetTop;
-
-//   e.target.style.setProperty('--x', `${x}px`);
-//   e.target.style.setProperty('--y', `${y}px`);
-// };
