def17dbb8a41ce4783555d777d120ba05aa05e0f
OlhaArtemenko <global14072012@gmail.com>
2024-01-04 12:07:15 +0200

    мобільне меню закривається при натисканні на посилання-якір

--- src/js/mobil-menu.js
+++ src/js/mobil-menu.js
@@ -3,10 +3,18 @@
     openModalBtn: document.querySelector('[data-mobil-menu-open]'),
     closeModalBtn: document.querySelector('[data-mobil-menu-close]'),
     modal: document.querySelector('[data-mobil-menu]'),
+    menuLinks: document.querySelectorAll('.mob-menu-navigation-link'), // Додано цей рядок
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  // Додаємо обробники подій для кожного посилання в меню
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      toggleModal(); // Закриваємо меню при кліці на посилання
+    });
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
